dbf2084fd856fd4641c73ffa470d5b3a750cb336
Andy Le <andy2639le@gmail.com>
2026-06-29 16:52:58 -0500

    Add http provider for IP auto-detection

--- infra/main.tf
+++ infra/main.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
   }
 }
 
@@ -15,4 +20,34 @@ provider "aws" {
 
 resource "aws_s3_bucket" "corpus" {
   bucket = "devtools-agent-corpus-andy-2026"
+}
+
+# Look up the default VPC that came with your account (read-only, not created)
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "http" "my_ip" {
+  url = "https://checkip.amazonaws.com"
+}
+
+resource "aws_security_group" "rds" {
+  name        = "devtools-agent-rds"
+  description = "Allows Postgres access from my current IP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Postgres from my current public IP"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
